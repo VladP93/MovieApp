a06f1f9c17c3a802887aaebb27f7ab76b7a3ed5a
Vladimir Paniagua <vladimirpaniagua@gmail.com>
2020-09-22 01:14:01 -0600

    Dark/Light theme added

--- App.js
+++ App.js
@@ -1,17 +1,52 @@
-import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
-import {Provider as PaperProvider} from 'react-native-paper';
-import {Button, Card} from 'react-native-paper';
-import {NavigationContainer} from '@react-navigation/native';
+import React, {useState, useMemo} from 'react';
+import {StyleSheet, StatusBar} from 'react-native';
+import {
+  Provider as PaperProvider,
+  DarkTheme as DarkPaper,
+  DefaultTheme as DefaultPaper,
+} from 'react-native-paper';
+import {
+  NavigationContainer,
+  DarkTheme as DarkNavigation,
+  DefaultTheme as DefaultNavigation,
+} from '@react-navigation/native';
 import Navigation from './src/navigation/Navigation';
+import PreferencesContext from './src/context/PreferencesContext';
 
 export default function App() {
+  const [theme, setTheme] = useState('dark');
+
+  DefaultPaper.colors.primary = '#1ae1f2';
+  DarkPaper.colors.primary = '#1ae1f2';
+  DarkPaper.colors.accent = '#1ae1f2';
+
+  DarkNavigation.colors.background = '#192734';
+  DarkNavigation.colors.card = '#15212b';
+
+  const toggleTheme = () => {
+    setTheme(theme === 'dark' ? 'light' : 'dark');
+  };
+
+  const preference = useMemo(
+    () => ({
+      toggleTheme,
+      theme,
+    }),
+    [theme],
+  );
+
   return (
-    <PaperProvider>
-      <NavigationContainer>
-        <Navigation />
-      </NavigationContainer>
-    </PaperProvider>
+    <PreferencesContext.Provider value={preference}>
+      <PaperProvider theme={theme === 'dark' ? DarkPaper : DefaultPaper}>
+        <StatusBar
+          barStyle={theme === 'dark' ? 'light-content' : 'dark-content'}
+        />
+        <NavigationContainer
+          theme={theme === 'dark' ? DarkNavigation : DefaultNavigation}>
+          <Navigation />
+        </NavigationContainer>
+      </PaperProvider>
+    </PreferencesContext.Provider>
   );
 }
 
--- src/navigation/DrawerContent.js
+++ src/navigation/DrawerContent.js
@@ -1,11 +1,13 @@
 import React, {useState} from 'react';
-import {StyleSheet} from 'react-native';
+import {StyleSheet, View} from 'react-native';
 import {DrawerContentScrollView} from '@react-navigation/drawer';
 import {Drawer, Switch, TouchableRipple, Text} from 'react-native-paper';
+import usePreference from '../hooks/usePreferences';
 
 export default function DrawerContent(props) {
   const {navigation} = props;
   const [active, setActive] = useState('home');
+  const {theme, toggleTheme} = usePreference();
 
   const onChangeScreen = (screen) => {
     setActive(screen);
@@ -31,8 +33,24 @@ export default function DrawerContent(props) {
           onPress={() => onChangeScreen('popular')}
         />
       </Drawer.Section>
+      <Drawer.Section title="Opciones">
+        <TouchableRipple>
+          <View style={styles.preferences}>
+            <Text>Tema oscuro</Text>
+            <Switch value={theme === 'dark'} onValueChange={toggleTheme} />
+          </View>
+        </TouchableRipple>
+      </Drawer.Section>
     </DrawerContentScrollView>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  preferences: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingVertical: 12,
+    paddingHorizontal: 16,
+  },
+});

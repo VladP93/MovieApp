0f8a4f8c2807f04285425d16c198f76cc65cdb60
Vladimir Paniagua <vladimirpaniagua@gmail.com>
2020-09-22 00:28:16 -0600

    Navigation/Screens added

--- App.js
+++ App.js
@@ -2,26 +2,15 @@ import React from 'react';
 import {StyleSheet, Text, View} from 'react-native';
 import {Provider as PaperProvider} from 'react-native-paper';
 import {Button, Card} from 'react-native-paper';
+import {NavigationContainer} from '@react-navigation/native';
+import Navigation from './src/navigation/Navigation';
 
 export default function App() {
-  console.log('Hola mundo en clg');
   return (
     <PaperProvider>
-      <View>
-        <Text>Hola Mundo </Text>
-        <Button
-          icon="camera"
-          mode="contained"
-          onPress={() => console.log('Pressed')}>
-          Press me
-        </Button>
-        <Card>
-          <Card.Actions>
-            <Button>Cancel</Button>
-            <Button>Ok</Button>
-          </Card.Actions>
-        </Card>
-      </View>
+      <NavigationContainer>
+        <Navigation />
+      </NavigationContainer>
     </PaperProvider>
   );
 }
